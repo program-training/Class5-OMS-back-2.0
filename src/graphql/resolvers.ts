4bf695e877edecefb54e056dda29ba35184fb581
eliya duek <eliya@example.com>
2023-12-04 11:50:08 +0200

    f

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,8 @@
+import recipesQueries from "../recipes/queries/recipesQueries";
+import booksQueries from "../books/queries/booksQueries";
+
+const resolvers = {
+  Query: {},
+};
+
+export default resolvers;
