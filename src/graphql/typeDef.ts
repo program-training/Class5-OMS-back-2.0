cd1d5b5090c5f93c8e50c4d25e3beeb20d3a3e26
eliya duek <eliya@example.com>
2023-12-04 12:10:23 +0200

    ff

--- src/graphql/typeDef.ts
+++ src/graphql/typeDef.ts
@@ -1,9 +1,3 @@
-import recipeTypes, {
-  recipesTypeQueries,
-} from "../recipes/typeDef/recipeTypes";
-import bookTypes, { booksTypeQueries } from "../books/typeDefs/booksTypes";
-import booksTypes from "../books/typeDefs/booksTypes";
-
 export const typeDefs = `#graphql   
   
     ${booksTypes}
